ed049464502ca931650dec5f709a44d4e40810db
Chowarin Chaikeunkeaw <chowarin@hotmail.com>
2023-08-21 21:26:11 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webaspmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
